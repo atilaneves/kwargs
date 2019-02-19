1b2fc46f0b3cb5c0654e27e13466d645712e2830
Atila Neves <atila.neves@gmail.com>
2019-02-19 12:32:28 +0100

    Refactor static foreach loop body into nthParam

--- source/kwargs/runtime.d
+++ source/kwargs/runtime.d
@@ -38,27 +38,31 @@ template kwargify(alias Function) if(!__traits(isTemplate, Function)) {
         static assert(wrongTypes.length == 0,
                       text("ERROR: ", wrongTypes.stringof, " are not parameters of ", __traits(identifier, Function)));
 
-        // the parameters to pass to the wrapped function
-        Tuple!funcArgTypes params;
+        auto nthParam(int index)() {
+            alias Type = typeof(params[index]);
 
-        static foreach(i; 0 .. Parameters!Function.length) {{
-
-            alias Type = typeof(params[i]);
-
-            enum hasDefaultValue = !is(ParameterDefaults!Function[i] == void);
+            enum hasDefaultValue = !is(ParameterDefaults!Function[index] == void);
             enum typeIndex = staticIndexOf!(Type, A);
 
             static if(typeIndex == -1) {
                 static assert(hasDefaultValue,
                               text("Could not find `", Type.stringof, "` in call to ", __traits(identifier, Function)));
-                params[i] = ParameterDefaults!Function[i];
+                return ParameterDefaults!Function[index];
             } else {
                 enum numTypes = numTypes!(Type, A);
                 static assert(numTypes == 1,
                               text("ERROR: found ", numTypes, " `", Type.stringof, "`s instead of 1"));
-                params[i] = args[typeIndex];
+                return args[typeIndex];
             }
-        }}
+        }
+
+        // the parameters to pass to the wrapped function
+        Tuple!funcArgTypes params;
+
+        static foreach(i; 0 .. Parameters!Function.length) {
+            params[i] = nthParam!i;
+        }
+
 
         // to avoid a static if on the return type
         auto call() {
